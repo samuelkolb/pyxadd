b7fe62f414d3495e14c1cfbad4aac086fb57fb34
Samuel Kolb <samuel.kolb@me.com>
2016-07-22 12:16:55 +0200

    Committing restructuring

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyxadd',
+      version='0.1',
+      description='A python package for extended algebraic decision diagrams',
+      url='http://github.com/samuelkolb/py-xadd',
+      author='Samuel Kolb',
+      author_email='samuel.kolb@me.com',
+      license='MIT',
+      packages=['pyxadd', 'tests'],
+      zip_safe=False)
