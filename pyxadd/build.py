deca2eb584f80a06005eb88d25da96ffe4c28a7a
Samuel Kolb <samuel.kolb@me.com>
2016-10-26 12:12:37 +0200

    Easier build methods

--- pyxadd/build.py
+++ pyxadd/build.py
@@ -11,6 +11,13 @@ class Builder(object):
     def pool(self):
         return self._pool
 
+    def vars(self, v_type, *args):
+        for arg in args:
+            self.pool.add_var(arg, v_type)
+
+    def ints(self, *args):
+        self.vars("int", *args)
+
     def terminal(self, exp):
         if exp == "1" or exp == 1:
             node_id = self.pool.one_id
@@ -19,6 +26,9 @@ class Builder(object):
         else:
             node_id = self.pool.terminal(exp)
         return Diagram(self.pool, node_id)
+
+    def exp(self, exp):
+        return self.terminal(exp)
 
     def test(self, lhs, symbol, rhs):
         return Diagram(self.pool, self.pool.internal(Test(lhs, symbol, rhs), self.pool.one_id, self.pool.zero_id))
@@ -35,5 +45,6 @@ class Builder(object):
         assert isinstance(if_diagram, Diagram)
         assert isinstance(then_diagram, Diagram)
         assert isinstance(else_diagram, Diagram)
+        assert if_diagram.pool == then_diagram.pool == else_diagram.pool == self.pool
 
         return if_diagram * then_diagram + (~if_diagram) * else_diagram
