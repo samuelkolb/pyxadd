7496ccb136a64e3d20601af354932cc78859e337
Samuel Kolb <samuel.kolb@me.com>
2016-12-13 17:48:57 +0100

    Refactored optimization into functions, and experiment into test

--- pyxadd/optimization.py
+++ pyxadd/optimization.py
@@ -1,14 +1,6 @@
-"""
-
-per node:
-n + n! = sum of parents or 1 if root
-
-NEED:
-parents per node
-
-"""
-from pyxadd.build import Builder
-from pyxadd.diagram import Pool
+import picos
+from pyxadd.diagram import TerminalNode, InternalNode
+from pyxadd.sympy_conversion import SympyConverter
 from pyxadd.walk import BottomUpWalker
 
 
@@ -32,3 +24,94 @@ class ParentsWalker(BottomUpWalker):
     def walk(self):
         BottomUpWalker.walk(self)
         return self.parents
+
+
+class PicosConverter(SympyConverter):
+    def __init__(self, registration):
+        self.registration = registration
+
+    def custom(self, value):
+        raise RuntimeError("Could not convert {} of type {}".format(value, type(value)))
+
+    def int(self, value):
+        return value
+
+    def times_batch(self, values):
+        return super(PicosConverter, self).times_batch([1] + values)
+
+    def times(self, val1, val2):
+        return val1 * val2
+
+    def float(self, value):
+        return value
+
+    def symbol(self, value):
+        return self.registration(value)
+
+    def add_batch(self, values):
+        return super(PicosConverter, self).add_batch([0] + values)
+
+    def add(self, val1, val2):
+        return val1 + val2
+
+
+def find_optimum(diagram, maximize=True, m=1000000):
+    problem = picos.Problem()
+    parent_map = ParentsWalker(diagram).walk()
+    variables = dict()
+    assignment_variables = []
+
+    def registration(value, v_type="integer"):
+        if value not in variables:
+            variables[value] = problem.add_variable(value, size=1, vtype=v_type)
+            assignment_variables.append(value)
+        return variables[value]
+
+    converter = PicosConverter(registration)
+
+    for node_id in parent_map:
+        node = diagram.pool.get_node(node_id)
+        if isinstance(node, TerminalNode):
+            variables[node_id] = problem.add_variable("t{}".format(node_id), vtype="binary")
+        elif isinstance(node, InternalNode):
+            variables[(node_id, True)] = problem.add_variable("i{}t".format(node_id), vtype="binary")
+            variables[(node_id, False)] = problem.add_variable("i{}f".format(node_id), vtype="binary")
+        else:
+            raise RuntimeError("Unknown node {} of type {}".format(node, type(node)))
+
+    s = problem.add_variable("s", vtype="integer")
+    problem.set_objective("max" if maximize else "min", s)
+
+    for node_id, parents in parent_map.items():
+        if len(parents) > 0:
+            parent_sum = 0
+            for i in range(0, len(parents)):
+                parent_sum += variables[parents[i]]
+        else:
+            parent_sum = 1
+
+        if node_id in variables:
+            problem.add_constraint(variables[node_id] == parent_sum)
+        else:
+            problem.add_constraint(variables[(node_id, True)] + variables[(node_id, False)] == parent_sum)
+
+    for node_id in parent_map:
+        node = diagram.pool.get_node(node_id)
+        if isinstance(node, InternalNode):
+            true_test = node.test.operator
+            lhs = 0
+            for var, coefficient in true_test.lhs.items():
+                lhs += registration(var) * coefficient
+            problem.add_constraint(lhs < (1 - variables[(node_id, True)]) * m + true_test.rhs)
+
+            false_test = (~node.test.operator).to_canonical()
+            lhs = 0
+            for var, coefficient in false_test.lhs.items():
+                lhs += registration(var) * coefficient
+            problem.add_constraint(lhs < (1 - variables[(node_id, False)]) * m + false_test.rhs)
+        elif isinstance(node, TerminalNode):
+            problem.add_constraint(s < (1 - variables[node_id]) * m + converter.convert(node.expression))
+
+    problem.solve(verbose=False)
+    return s.value[0, 0], {v: variables[v].value[0, 0] for v in assignment_variables}
+
---
+++ tests/test_ilp_opt.py
@@ -0,0 +1,26 @@
+import unittest
+
+from pyxadd.build import Builder
+from pyxadd.diagram import Pool
+from pyxadd.optimization import find_optimum
+from pyxadd.view import export
+
+
+def build_diagram1():
+    pool = Pool()
+    b = Builder(pool)
+    b.ints("x")
+    return b.ite(b.test("x", "<=", 3), b.ite(b.test("x", "<=", 2), b.exp(1), b.exp("2*x")), b.exp(1))
+
+
+delta = 10 ** -6
+
+
+class TestIlpOpt(unittest.TestCase):
+    def test_diagram1(self):
+        diagram = build_diagram1()
+        export(diagram, "visual/ilp_opt/d.dot")
+        value, assignment = find_optimum(diagram)
+        self.assertAlmostEqual(6.0, value, delta=delta)
+        self.assertEqual(1, len(assignment))
+        self.assertAlmostEqual(3.0, assignment["x"], delta=delta)
