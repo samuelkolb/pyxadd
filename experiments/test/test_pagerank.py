0a580e9101697dd26a925494d41d3dd0c4a1da01
Samuel Kolb <samuel.kolb@me.com>
2017-01-13 12:40:43 +0100

    Implemented flexible pagerank testing

--- experiments/test/test_pagerank.py
+++ experiments/test/test_pagerank.py
@@ -46,9 +46,11 @@ def pagerank_ground(matrix_a, damping_factor, iterations, delta, initial_vector=
 
 class TestPagerank(unittest.TestCase):
     def setUp(self):
-        pass
+        self.print_time = False
+        self.delta = 10 ** -3
+        self.damping_factor = 0.85
 
-    def test_build_example1(self):
+    def build_example1(self):
         pool = Pool()
         build = Builder(pool)
         variables = [("i", 1, 4)]
@@ -61,11 +63,18 @@ class TestPagerank(unittest.TestCase):
         column3 = (build.limit("c_i", 3, 3) & build.limit("r_i", 1, 1)) * build.exp("1")
         column4 = (build.limit("c_i", 4, 4) & (build.limit("r_i", 1, 1) | build.limit("r_i", 3, 3))) * build.exp("1/2")
         diagram = limits * (column1 + column2 + column3 + column4)
+        return diagram, variables
+
+    def test_example1(self):
+        diagram, variables = self.build_example1()
+        self._test_compare(diagram, variables, 100, self.damping_factor)
+
+    def _test_compare(self, diagram, variables, iterations, damping_factor):
         row_variables = [("r_" + name, lb, ub) for name, lb, ub in variables]
         column_variables = [("c_" + name, lb, ub) for name, lb, ub in variables]
         matrix = Matrix(diagram, row_variables, column_variables).reduce()
-        pagerank.page_rank(matrix, variables=variables, iterations=100)
+        pagerank.page_rank(matrix, variables, damping_factor=damping_factor, iterations=iterations, delta=self.delta)
 
-    def test_example1(self):
-        matrix = numpy.matrix([[0, 0, 1, 0.5], [1 / 3.0, 0, 0, 0], [1 / 3.0, 0.5, 0, 0.5], [1 / 3.0, 0.5, 0, 0]])
-        pagerank_ground(matrix, 0.85, 100, 10 ** -3)
+        ground_matrix = matrix.to_ground()
+        pagerank_ground(ground_matrix, damping_factor=damping_factor, iterations=iterations, delta=self.delta)
+
