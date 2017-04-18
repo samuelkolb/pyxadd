4b1322314d6a7378a01fc945210241e10a2bb9e9
Samuel Kolb <samuel.kolb@me.com>
2017-04-18 15:32:19 +0200

    Expanding resolution test

--- tests/test_resolution.py
+++ tests/test_resolution.py
@@ -15,6 +15,20 @@ class ResolutionTest(unittest.TestCase):
         resolved = operator_1.resolve("x", operator_2)
         print(resolved)
 
+    def test_resolution2(self):
+        x = sympy.sympify("x")
+        y = sympy.sympify("y")
+
+        # 1.0 <= y - 1
+        # x >= 1 resolve
+
+        operator_1 = Operator.compile(x, "<=", 1.0)
+        operator_2 = Operator.compile(x - y, "<=", -1)
+        resolved = operator_1.switch_direction().resolve("x", operator_2)
+        print(resolved)
+        # (1.0 * x <= 1.0)
+        # leq(-1.0 * y + 1.0 * x <= -1.0) = (1.0 * y <= 2.0)
+
 
 if __name__ == '__main__':
     unittest.main()
